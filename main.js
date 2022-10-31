232f5db2205c80bba8e4b3e8dcb0d783eb13a0cd
mathildamus <113375599+MariamMathildaAmonashvili@users.noreply.github.com>
2022-10-31 17:12:43 +0400

    Add files via upload
    Finish

---
+++ main.js
@@ -0,0 +1,77 @@
+
+//Task1 //
+const currentDay = new Date();
+const day = currentDay.getDay();
+
+
+switch 
+(currentDay) 
+{
+    case 0:
+    day = 'Sunday';
+    break;
+    case 1:
+        day = 'Monday';
+        break ;
+        case 2:
+            day = 'Tuesday';
+            break ; 
+            case 3:
+                day = 'Wednesday';
+                break;
+                case 4:
+                    day = 'Thursday';
+                    break;
+                    case 5:
+                        day = 'Friday' ;
+                        break;
+                        case 6:
+                            day = 'Saturday';
+                            
+
+}
+//console.log(currentDay);
+//console.log(day);
+
+//Task2
+let i = 0 ;
+for (let i = 0; i < 50 ; i ++) {
+
+//console.log (i);
+
+}
+
+
+//Task 3 
+
+let b = 0; 
+
+while (b < 150 ) { 
+    b++;
+    //console.log (b);
+}
+
+//Task 4 
+
+let k = 0;
+
+do {
+    //console.log(k);
+    k++ ;
+
+}
+while (k < 100 ) ;
+
+//Task 5
+
+let array = [ ] ;
+
+for (let m = 1 ; m <= 100 ; m++ ) 
+{  array.push (m*m) ; 
+
+// ეს უბრალოდ მაინტერესებდა რა შედეგს მომცემდა {}-ებს :D შიგნით console.log (array) ;
+
+}
+
+
+//console.log (array) ;
